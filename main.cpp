081cd5c574799b8488bf7c57d8e73ca00add681b
Benton-Lane <bentonlane@gmail.com>
2026-05-11 14:16:26 -0400

    Adding in some functionality for assets
    - Added folders to "resources" folder for fonts and sprites
    - Made sure the pathing was correct in the test main file
    - Werks

--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@ int main() {
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
+	// Test to create new font
+	Font test_font = LoadFontEx("Fonts/Playwrite.ttf", 32, 0, 250);
+	Vector2 textPos = { 200, 200 };
+
+	// Load a texture from the resources directory
+	Texture wabbit = LoadTexture("Sprites/wabbit_alpha.png");
+
 	// game loop
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
 	{
@@ -22,7 +29,13 @@ int main() {
 		ClearBackground(BLUE);
 
 		// draw some text using the default font
-		DrawText("Hey LOSER", 200, 200, 40, ORANGE);
+		//DrawText("Hey LOSER", 200, 200, 40, ORANGE);
+
+		// draw some text with a new font
+		DrawTextEx(test_font, "Hey LOSER", Vector2{ 200, 200 }, test_font.baseSize, 2, ORANGE);
+
+		// draw our texture to the screen
+		DrawTexture(wabbit, 400, 200, WHITE);
 
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
